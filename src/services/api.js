7709245481dd3fde380efbfb16514b0d525103ab
nandhuselvam <151752690+nandhuselvam@users.noreply.github.com>
2024-03-27 12:16:43 +0530

    Add files via upload

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
 
-const API_URI = 'http://localhost:8000'
+const API_URI = 'http://localhost:8000';
 
 const API_GMAIL = async (serviceUrlObject, requestData = {}, type) => {
-    const { params, urlParams, ...body } = requestData;
+    const { urlParams, body } = requestData;
 
-    return await axios({
-        method: serviceUrlObject.method,
-        url: `${API_URI}/${serviceUrlObject.endpoint}/${type}`,
-        data: requestData
-    })
-}
+    // Constructing URL with urlParams
+    let url = `${API_URI}/${serviceUrlObject.endpoint}/${type}`;
+    if (urlParams) {
+        url += `/${urlParams}`;
+    }
+
+    try {
+        const response = await axios({
+            method: serviceUrlObject.method,
+            url: url,
+            data: body // Passing body instead of requestData
+        });
+        return response.data;
+    } catch (error) {
+        // Handle error here if needed
+        console.error('Error in API_GMAIL:', error);
+        throw error; // Rethrow error to be handled by the caller
+    }
+};
 
 export default API_GMAIL;
